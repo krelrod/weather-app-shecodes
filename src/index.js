d01d3d1935a927a1e4498a8240a21027f9b4f476
krelrod <k.annelrod@gmail.com>
2020-09-30 00:59:42 -0400

    Updated CSS, added weather icons, created a search engine.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ function displayTemp(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windspeedElement = document.querySelector("#windspeed");
+  let conditionIconElement = document.querySelector("#condition-icon");
+  conditionIconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   countryElement.innerHTML = response.data.sys.country;
@@ -14,55 +19,68 @@ function displayTemp(response) {
   windspeedElement.innerHTML = Math.round(response.data.wind.speed);
   console.log(response.data);
 }
-let apiKey = "1a11d8dac18dce724ebbebc6a9526d87";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Decatur&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemp);
+function search(city) {
+  let apiKey = "1a11d8dac18dce724ebbebc6a9526d87";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemp);
 
-function currentTime() {
-  let time = new Date();
+  function submitSearch(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
 
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+    search(cityInputElement.value);
+  }
+  search(Decatur);
 
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+  let form = document.querySelector("#search-form");
+  form.addEventListener("submit", submitSearch);
 
-  let currentMonth = months[time.getMonth()];
-  let currentDay = days[time.getDay()];
-  let currentDate = time.getDate();
-  let currentYear = time.getFullYear();
-  let currentHour = time.getHours();
-  let currentMinutes = time.getMinutes();
+  function currentTime() {
+    let time = new Date();
 
-  if (currentHour < 10) {
-    currentHour = `0${currentHour}`;
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    let currentMonth = months[time.getMonth()];
+    let currentDay = days[time.getDay()];
+    let currentDate = time.getDate();
+    let currentYear = time.getFullYear();
+    let currentHour = time.getHours();
+    let currentMinutes = time.getMinutes();
+
+    if (currentHour < 10) {
+      currentHour = `0${currentHour}`;
+    }
+
+    if (currentMinutes < 10) {
+      currentMinutes = `0${currentMinutes}`;
+    }
+
+    return `${currentDay}, ${currentMonth} ${currentDate}, ${currentYear} | ${currentHour}:${currentMinutes}`;
   }
 
-  if (currentMinutes < 10) {
-    currentMinutes = `0${currentMinutes}`;
-  }
-
-  return `${currentDay}, ${currentMonth} ${currentDate}, ${currentYear} | ${currentHour}:${currentMinutes}`;
+  let fullTimeShown = document.querySelector("#current-time");
+  fullTimeShown.innerHTML = currentTime();
 }
-
-let fullTimeShown = document.querySelector("#current-time");
-fullTimeShown.innerHTML = currentTime();
